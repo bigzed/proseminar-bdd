960a1495e55abcb1c5f22d191421817e58759c4a
Steve Dierker <kiwi@obstkiste.org>
2013-04-27 20:14:01 +0200

    Corrected multiple authors in sources Removed unecessary files from lib

--- doc/lib/calculator_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Calculator do
-  context 'addition' do
-    it 'should add correctly' do
-      @calc.push 3
-      @calc.push 4
-      @calc.add.should == 7
-    end
-  end
-end
--- doc/lib/calculator_test.rb
+++
@@ -1,8 +0,0 @@
-class CalculatorTest < Test::Unit::TestCase
-  def test_addition
-    calculator = Calculator.new
-    calculator.push 3
-    calculator.push 4
-    assert_equal(calculator.add, 7)
-  end
-end
